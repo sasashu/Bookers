839e267dde3724671a91715641c8edb88d18fb3f
EC2 Default User <ec2-user@ip-172-31-38-185.ap-northeast-1.compute.internal>
2022-09-07 10:54:47 +0000

    [Add]create機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,41 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-  
+
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to '/books/show'
+    else
+      render :index
+    end
   end
 
   def index
+    @book = Book.all
   end
 
   def show
+    # @book = Book.find(params[:id])
   end
 
   def edit
+    # @book = Book.find(params[:id])
   end
-  
+
   def update
+    # @book = Book.find(params[:id])
+    # book.update(list.params)
+    # redirect_to book_path(book.id)
   end
-  
+
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
   end
 end
